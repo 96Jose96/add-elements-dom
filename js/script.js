c0d3d96c61625167dd04a3258081240bad939127
Jose <jm.diaz6620@gmail.com>
2024-06-12 00:38:57 +0200

    terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
  // Aquí tu código
+const element = document.getElementById('agregar');
+let lista = document.getElementById('lista');
+
+agregar.addEventListener('click', function() {
+    const elemento = prompt('Escribe un elemento');
+    const newElement = document.createElement('li');
+    newElement.innerText = elemento;
+    lista.appendChild(newElement);
+})
